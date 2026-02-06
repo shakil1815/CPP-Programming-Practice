cb80bc1cd42fc2c8d5f8438c9db5da155b4680d2
Shakil Ahmed <shakil.ahmed.mobin@gmail.com>
2026-02-06 10:50:32 +0600

    2nd commit

---
+++ Loops/basicLoops.cpp
@@ -0,0 +1,10 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int n;
+    cout<<"Enter number:";
+    cin>>n;
+    for(int i=0;i<=n;i++){
+        cout<<"Hello World!!"<<endl;
+    }
+}
